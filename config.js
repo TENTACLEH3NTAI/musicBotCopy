3d4291b8153415bce6d60ea3a707d0136607b545
TENTACLEH3NTAI <41243359+TENTACLEH3NTAI@users.noreply.github.com>
2018-08-19 20:58:50 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = '';
+exports.TOKEN = 'NDc5Mjg1MzI0NTgxNjk5NjA1.DlnOYg.7TscrHuVIKE0ennm8sV4iNng55c';
 
-exports.PREFIX = 'm!';
+exports.PREFIX = 'don';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyBztRBQ54yKmodgyjWZvjWdj-uECiCVL2A';
